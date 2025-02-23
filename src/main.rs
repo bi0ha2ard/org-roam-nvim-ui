a475db9891c5efcedb0274529cf538e129a7c333
Felix <felix@fzeltner.de>
2025-02-24 00:11:32 +0100

    unhardcode username

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-use serde::{de::IgnoredAny, Deserialize};
+use serde::{Deserialize, de::IgnoredAny};
 
 #[derive(Deserialize)]
 #[serde(untagged)]
@@ -23,18 +23,26 @@ struct Node {
     id: String,
     level: i32,
     title: String,
-    mtime: u64
+    mtime: u64,
 }
 
 fn main() {
-    let file = std::fs::File::open("/home/felix/.local/share/nvim/org-roam.nvim/db_pretty.json").expect("Open");
+    const DB_FNAME: &str = "db_pretty.json";
+    const ORG_ROAM_SHARE_DIR: &str = ".local/share/nvim/org-roam.nvim";
+    let roam_share_loc = std::path::Path::new(&std::env::var_os("HOME").expect("home"))
+        .join(ORG_ROAM_SHARE_DIR)
+        .join(DB_FNAME);
+    let file = std::fs::File::open(roam_share_loc).expect("Open");
     let db: Database = serde_json::from_reader(std::io::BufReader::new(file)).expect("Parse");
     for n in db.nodes.values() {
         println!("{}: {}", n.title, n.id);
     }
-    for (k, v) in db.inbound {
+
+    // TODO: put directly into multimap
+    // TODO: parse UUIDs to avoid String?
+    for (k, v) in &db.inbound {
         match v {
-            Link::Empty(_) => {},
+            Link::Empty(_) => {}
             Link::Links(links) => {
                 for t in links.keys() {
                     println!("{} <- {}", k, t);
@@ -42,9 +50,9 @@ fn main() {
             }
         }
     }
-    for (k, v) in db.outbound {
+    for (k, v) in &db.outbound {
         match v {
-            Link::Empty(_) => {},
+            Link::Empty(_) => {}
             Link::Links(links) => {
                 for t in links.keys() {
                     println!("{} -> {}", k, t);
@@ -52,4 +60,6 @@ fn main() {
             }
         }
     }
+
+    println!("{}, {}, {}", db.nodes.keys().count(), db.inbound.keys().count(), db.outbound.keys().count());
 }
