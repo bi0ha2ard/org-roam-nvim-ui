775b8fe788f589a228a62a1d26e41139bf96bb62
Felix <felix@fzeltner.de>
2025-02-28 23:23:15 +0100

    untested graph layouting

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use itertools::Itertools;
+use nalgebra::{Point2, Vector2};
+use rand::SeedableRng;
 use std::collections::HashMap;
 
 use serde::{Deserialize, de::IgnoredAny};
@@ -103,6 +106,10 @@ impl Graph {
             .map(|l| self.nodes.get(l.to).unwrap())
     }
 
+    fn is_connected(&self, from: usize, to: usize) -> bool {
+        self.links.iter().find(|l| (l.from == from && l.to == to) || (l.from == to && l.to == from)).is_some()
+    }
+
     fn dot(&self) -> String {
         let mut res = String::new();
         res.push_str("digraph {");
@@ -125,6 +132,71 @@ impl Graph {
         }
         res.push('}');
         res
+    }
+}
+
+type Point = Point2<f32>;
+type Vector = Vector2<f32>;
+
+struct PlacedNode {
+    p: Point,  // Location
+    f: Vector, // yet to be applied force
+    id: usize, // keep track of id in case we're filtered down later
+}
+
+struct GraphLayout {
+    // Current positions
+    nodes: Vec<PlacedNode>,
+    rng: rand::rngs::StdRng,
+}
+
+impl GraphLayout {
+    fn new(nodes: &[Node], seed: u64) -> GraphLayout {
+        GraphLayout {
+            nodes: nodes
+                .iter()
+                .map(|n| PlacedNode {
+                    p: Point::origin(),
+                    f: Vector::zeros(),
+                    id: n.id,
+                })
+                .collect(),
+            rng: rand::rngs::StdRng::seed_from_u64(seed)
+        }
+    }
+
+    fn tick(&mut self, graph: &Graph, dt: f32) {
+        const MIN_DIST_FOR_DIR: f32 = 1e-6;
+        const DIST_FOR_LINKS: f32 = 1.0;
+        let distribution = rand::distributions::Uniform::<f32>::new_inclusive(-1.0, 1.0);
+        for n in &mut self.nodes {
+            n.f = Vector::zeros();
+        }
+        for (me, other) in (0..self.nodes.len()).tuple_combinations() {
+            let displacement = {
+                let me = self.nodes.get(me).unwrap();
+                let other = self.nodes.get(other).unwrap();
+                other.p - me.p
+            };
+            let dist = displacement.norm_squared();
+            let dir: Vector = if dist.abs() < MIN_DIST_FOR_DIR {
+                Vector::from_distribution(&distribution, &mut self.rng).normalize()
+            } else {
+                displacement.normalize()
+            };
+            // Repelling
+            self.nodes[other].f += dt * dir.scale(1. / dist);
+            self.nodes[me].f += dt * dir.scale(-1. / dist);
+            // Attracting
+            if dist > DIST_FOR_LINKS && graph.is_connected(me, other) {
+                self.nodes[other].f -= dt * dir.scale(1. / dist);
+                self.nodes[me].f -= dt * dir.scale(-1. / dist);
+            }
+        }
+        for n in &mut self.nodes {
+            // TODO: clamp forces
+            n.p += n.f;
+        }
     }
 }
 
